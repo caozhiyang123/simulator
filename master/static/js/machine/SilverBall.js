f241fc4c21f0f9cc436b4e289394195f3ce3205f
caozhiyang123 <1250233433@qq.com>
2026-07-14 10:27:42 +0800

    create machine SilverBall

---
+++ master/static/js/machine/SilverBall.js
@@ -0,0 +1,7 @@
+// ---------------------------------------------------------------------------
+// SilverBall Machine Plugin (Bingo)
+// 5x3 cards, highest_win pattern matching.
+// ---------------------------------------------------------------------------
+MachineRegistry.register('SilverBall', {
+  type: 'bingo'
+});
